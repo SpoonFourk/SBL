6e69cbf6a2e63e8ce0df7bb6321311258de47791
Mima-Dozh <d.k.mozhaev12345@gmail.com>
2022-04-01 15:49:06 +0500

    Add Player code

--- Player.cs
+++ Player.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float moveSpeed = 100;
-    private Rigidbody2D rigidBody;
+    //private float acceleration = 3;
+    //private Rigidbody rigidBodyComponent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var w = Input.GetKey(KeyCode.W) ? 1 : 0;
+        /*var w = Input.GetKey(KeyCode.W) ? 1 : 0;
         var s = Input.GetKey(KeyCode.S) ? -1 : 0;
         var a = Input.GetKey(KeyCode.A) ? 1 : 0;
         var d = Input.GetKey(KeyCode.D) ? -1 : 0;
         transform.position = Vector3.zero;
         var movementVector = new Vector2(-(a + d), w + s);
         rigidBodyComponent.velocity = movementVector * acceleration;
-    }
+*/    }
 }
