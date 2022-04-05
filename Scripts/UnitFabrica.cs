11a76521616486400cdb69dfa98dbb482a02c282
Mima-Dozh <d.k.mozhaev12345@gmail.com>
2022-04-05 12:10:05 +0500

    Add spawn count

--- Scripts/UnitFabrica.cs
+++ Scripts/UnitFabrica.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class UnitFabrica : MonoBehaviour
 {
+    [SerializeField] private int unitCount = 16;
     private bool a;
     // Start is called before the first frame update
     void Start()
@@ -18,10 +19,11 @@ public class UnitFabrica : MonoBehaviour
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var len = mousePosition - transform.position;
         if(Input.GetMouseButtonDown(0)
-        &&
-            Mathf.Abs(len.x) < 1
+        && unitCount > 0
+        &&  Mathf.Abs(len.x) < 1
         && Mathf.Abs(len.y) < 1)
         {
+            unitCount--;
             UnitSelect.SetDeselect();
             var x = GameObject.Find("PapaPotato");
             Instantiate(x, transform.position + Vector3.left, Quaternion.identity);
